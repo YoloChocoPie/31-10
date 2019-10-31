433471b8ea9272606bf34950b4b407576a45be3b
YoloChocoPie <chocopieyolo@gmail.com>
2019-10-31 15:14:16 +0700

    Book and XY- Object Oriented 31-10

---
+++ ConsoleApplication2/ConsoleApplication2/mypoint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class mypoint
+    {
+        private int X, Y;
+        public mypoint()
+        {
+            this.X = this.Y = 0;
+        }
+
+        public mypoint(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int GetX() { return this.X; }
+        public int GetY() { return this.Y; }
+
+        public void SetX(int x) { this.X = x; }
+        public void SetX(int y) { this.Y = y; }
+
+        public int[] GetXY()
+        {
+            return new int[] { this.X, this.Y };
+        }
+
+    }
+}
